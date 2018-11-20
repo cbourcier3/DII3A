5000af1f67f7a93ebfc74b936c754d81968e7ae1
cbourcier3 <celestin.bourcier@gmail.com>
2018-11-20 15:23:03 +0100

    test

---
+++ C/COURS DII 3A/pyramideCaractere.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *get_first_arg(int argc, char *argv[])
+{
+   if (argc <2)
+   {
+      printf("no argument\n\n");
+      exit(0);
+   }
+   return (argv[1]);
+}
+int main(int argc, char *argv[])
+{
+   char *arg = get_first_arg(argc, argv);   
+   int i=0;
+   int longueurChaine=strlen(arg);
+   printf("chaine: %s \n", arg);
+   printf("longueurChaine: %d \n",longueurChaine);
+   for(i=0;i<longueurChaine;i++)
+   {
+      for(int j=0;j<longueurChaine-i;j++)
+         printf(" ");
+      for(int j=i+1;j>0;j--)
+         printf("%c",arg[j-1]);
+      for(int j=1;j<i+1;j++)
+         printf("%c",arg[j]);
+      printf("\n");
+   }
+
+   return 0;
+}
---
+++ C/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int recupererNombre(int minimum, int maximum){
+    int nombre;
+    do{
+        printf("Entrez un nombre compris entre %d et %d :",minimum,maximum);
+        scanf("%d",&nombre);
+        if (nombre<minimum){
+
+        }
+    }while(1);
+}
+int main(int argc, char* argv[])
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+   printf("%")
+}

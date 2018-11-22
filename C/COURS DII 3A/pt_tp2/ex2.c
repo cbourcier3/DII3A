062455b155f2dd67f8fabacebcac8ef447005678
cbourcier3 <celestin.bourcier@gmail.com>
2018-11-22 17:54:27 +0100

    ex2finit

--- C/COURS DII 3A/pt_tp2/ex2.c
+++ C/COURS DII 3A/pt_tp2/ex2.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tp2.h"
+#include <string.h>
 
+char *get_first_arg(int argc, char *argv[])
+{
+   if (argc <2)
+   {
+      printf("no argument\n\n");
+      exit(0);
+   }
+   return (argv[1]);
+}
 int main(int argc, char *argv[])
 {
-    char *test= get_first_arg(argc,argv);
+    char *chaine=get_first_arg(argc,argv);
+    int compteur=0;
+    for (int i=0; i<strlen(chaine);i++)
+    {
+        if (chaine[i]==32)
+        {
+            if (compteur!=0){
+                printf("\n");
+            }
+            compteur=0;
+        }
+        else
+        {
+            if (compteur==0 && chaine[i]<123 && chaine[i]>96)
+                chaine[i]=chaine[i]-32;
+            printf("%c",chaine[i]);
+            compteur++;
+        }
+    }
     //char testA='a',testB='z',testC='A',testD='Z';
     //printf("a: %d , z: %d, A: %d, Z: %d",testA,testB,testC,testD);
-    test = upper(test);
     return 0;
 }
